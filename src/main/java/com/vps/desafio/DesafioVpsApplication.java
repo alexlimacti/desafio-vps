4662c4d76c078162143d97c2817890b01601ed3e
Alexsandro Lima Silva <alexsandrolima.dev@gmail.com>
2025-07-01 16:40:01 -0300

    feat: initial project structure

---
+++ src/main/java/com/vps/desafio/DesafioVpsApplication.java
@@ -0,0 +1,13 @@
+package com.vps.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioVpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioVpsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vps/desafio/DesafioVpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vps.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioVpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
